9f0bc24bb174bb1680d99d5fdb91c49a7e9007fd
Sergey <nikolaevsv1@yandex.ru>
2023-10-24 23:43:16 +0300

    task3 fin

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -3,5 +3,17 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Ведите номер дня недели: ");
-
+Console.WriteLine("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7 || number < 1)
+{
+    Console.WriteLine($"{number} -> Это не номер дня недели");
+}
+else if (number < 6)
+{
+    Console.WriteLine($"{number} -> будний день");
+}
+else
+{
+Console.WriteLine($"{number} -> выходой");
+}
