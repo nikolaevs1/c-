724fd8e2f7eacff76506c2dd1a6810f22e24b34e
Sergey <nikolaevsv1@yandex.ru>
2023-10-25 21:23:57 +0300

    begin autotest1

---
+++ autotest1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
