5e29066c26685958531ea832aebf687c31b82362
Sergey <nikolaevsv1@yandex.ru>
2023-10-25 22:15:13 +0300

    autotest1 fin

--- autotest1/Program.cs
+++ autotest1/Program.cs
@@ -2,8 +2,35 @@
 
 public class Answer
 {
-    static bool IsPalindrome(int number){
+    static bool IsPalindrome(int number)
+    {
       // Введите свое решение ниже
+      if (number >= 10000 && number < 100000)
+{
+    int firstDigit = number / 10000;
+    int lastDigit = number % 10;
+    int secondDigit = (number /1000)%10;
+    int forthDigit = number /10 %10;
+    if (firstDigit == lastDigit && secondDigit == forthDigit)
+    {
+        System.Console.Write($"{number} -> ");
+        return true;
+    }
+        else
+        {
+            System.Console.Write($"{number} -> ");
+            return false;
+        }
+        
+}
+else 
+{
+Console.WriteLine($"{number} -> Число не пятизначное");
+return false;
+}
+
+
+
       
     }
   
