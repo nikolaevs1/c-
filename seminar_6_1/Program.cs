e412b20857a055949f71c3956c3483730bce4888
Sergey <nikolaevsv1@yandex.ru>
2023-11-26 15:36:31 +0300

    domashka 1 seminar 6

---
+++ seminar_6_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [m];
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Введите число {i+1}: ");
+    numbers [i] = Convert.ToInt32 (Console.ReadLine());
+    if (numbers[i]>0)
+    {
+        count+=1;
+    }
+
+}
+PrintArray(numbers);
+Console.WriteLine($"Колличество чисел больше 0: {count}");
+
+
+static void PrintArray(int[] array)
+    {
+         // Введите свое решение ниже
+Console.WriteLine($"[{String.Join("\t", array)}]");
+
+    }
