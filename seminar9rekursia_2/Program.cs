d830f8a9d00934e0a46d631c771aa372a09a9508
Sergey <nikolaevsv1@yandex.ru>
2023-12-03 17:04:02 +0300

    domashka 2 seminar 9

---
+++ seminar9rekursia_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M > N)
+{
+    Console.Write("Число M не должно быть больше N");
+}
+else
+{
+    Console.WriteLine(PrintSumm(M, N));
+}
+
+
+int PrintSumm(int start, int end)
+{
+    // Базовый случай
+    if (start == end) return start;
+    // Рекурсивный случай
+    return (start + PrintSumm(start + 1, end));
+}
+
