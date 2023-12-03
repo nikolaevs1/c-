22d8b9726ebd01b9f601a780d606ff397e2d4986
Sergey <nikolaevsv1@yandex.ru>
2023-12-03 16:59:16 +0300

    dobavlena proverka

--- seminar9rekursia_1/Program.cs
+++ seminar9rekursia_1/Program.cs
@@ -2,19 +2,29 @@
 // которая выведет все натуральные числа в промежутке от M до N.
 // M = 1; N = 5. -> "1, 2, 3, 4, 5"
 // M = 4; N = 8. -> "4, 6, 7, 8"
+// M = 10; N = 58. -> "Число M не должно быть больше N"
 
 Console.Write("Введите число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-// start = 1, end = N(3) , start++
-// 1
+
+
+if (M > N)
+{
+    Console.Write("Число M не должно быть больше N");
+}
+else
+{
+    Console.WriteLine(PrintNumbers(M, N));  
+}
+
 string PrintNumbers(int start, int end)
 {
     // Базовый случай
     if (start == end) return start.ToString();
     // Рекурсивный случай
-    return (start + "\t" + PrintNumbers(start + 1, end));
+    return (start + ", " + PrintNumbers(start + 1, end));
 }
 
-Console.WriteLine(PrintNumbers(M, N));
+
